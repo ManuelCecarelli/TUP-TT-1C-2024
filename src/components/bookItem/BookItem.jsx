b0bbc5f3b103950fa902ef6e07fb6ee2d6fb0f01
ManuelCecarelli <manuelcecarelli@gmail.com>
2024-04-12 02:01:36 -0300

    Cambiamos contenido antiguo de BookItem x componente Card de react-bootstrap

--- src/components/bookItem/BookItem.jsx
+++ src/components/bookItem/BookItem.jsx
@@ -1,13 +1,21 @@
 import PropTypes from "prop-types";
+import { Card } from "react-bootstrap";
 
-const BookItem = ({ title, author, rating, pages }) => {
+const BookItem = ({ title, author, rating, pages, imageUrl }) => {
   return (
-    <>
-      <h2>{title}</h2>
-      <h3>{author}</h3>
-      <div>{rating?.length} estrellas</div>
-      <p>{pages} páginas</p>
-    </>
+    <Card style={{ width: "22rem" }}>
+      <Card.Img
+        height={400}
+        variant="top"
+        src={imageUrl !== "" ? imageUrl : "https://bit.ly/47NylZk"}
+      />
+      <Card.Body>
+        <Card.Title>{title}</Card.Title>
+        <Card.Subtitle>{author}</Card.Subtitle>
+        <div>{rating?.length} estrellas</div>
+        <p>{pages} páginas</p>
+      </Card.Body>
+    </Card>
   );
 };
 
